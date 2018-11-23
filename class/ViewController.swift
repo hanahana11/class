f9bf3036a062a9cfd336eddc04458f81793a554c
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2018-11-23 19:51:22 +0900

    Initial Commit

---
+++ class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  class
+//
+//  Created by 上田　護 on 2018/11/23.
+//  Copyright © 2018 mamoru.ueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
